102cca3af401e80d9ada27016dcdca2e5c1d1b5a
bhaveshpanjwani <99314638+bhaveshpanjwani@users.noreply.github.com>
2022-07-28 23:31:34 +0530

    first commit

---
+++ lib/ApiService/apiService.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+//mport '../model/article.dart';
+
+
+class ApiService{
+  final endpoint = "";
+  Future<List<Article>> getArticle() async{
+    Response response = await get(Uri.parse(endpoint));
+
+    if(response.statusCode == 200){
+      Map<String,dynamic> json = jsonDecode(response.body);
+      List<dynamic> body = json['articles'];
+      List<Article> articles = body.map((dynamic item) => Article.fromJson(item)).toList();
+      return articles;
+    }else{
+      throw('Data has Error');
+    }
+  }
+}
